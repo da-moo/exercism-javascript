c730d44584fd720506c57982f50d2753628897f9
Peter Dimou <git@damoo.zone>
2018-10-31 11:17:38 -0400

    Refactor to only use boolean operators

--- leap/leap.js
+++ leap/leap.js
@@ -1,9 +1,1 @@
-export const isLeap = year => {
-  if (year % 4 === 0) {
-    if (year % 100 === 0) {
-      return year % 400 === 0;
-    }
-    return true;
-  }
-  return false;
-};
+export const isLeap = year => year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
