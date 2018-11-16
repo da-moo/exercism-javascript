3c8d3ba00dc9ab9a8fa8034a42a99106670c0b1f
DaMoo <git@damoo.zone>
2018-11-16 16:28:10 -0500

    Refactor simple-cipher based on feedback
    Reflection: I wasn't aware Array.from had an overload with a mapping
    function as another parameter! Very useful. I also renamed constants
    with all caps and snake case to more effectively communincate their
    intent.

--- simple-cipher/simple-cipher.js
+++ simple-cipher/simple-cipher.js
@@ -1,3 +1,5 @@
+const mod = (n, m) => ((n % m) + m) % m;
+
 export const Cipher = class {
   constructor(key) {
     if ((key && !key.match(/^[a-z]+$/)) || key === '') {
@@ -7,46 +9,50 @@ export const Cipher = class {
   }
 
   static generateRandomKey() {
-    return Array.from(Array(Cipher.randomKeyLength))
-      .map(() => {
-        const charPosition = Math.floor(Math.random() * Cipher.possible.length);
-        return Cipher.possible[charPosition];
-      })
-      .join('');
+    return Array.from(Array(Cipher.RANDOM_KEY_LENGTH), () => {
+      const charPosition = Math.floor(
+        Math.random() * Cipher.POSSIBLE_KEY_CHARS.length
+      );
+      return Cipher.POSSIBLE_KEY_CHARS[charPosition];
+    }).join('');
   }
 
   encode(text) {
-    return this.applyCipher(text, 'encode');
+    return this.applyCipher(text, Cipher.ENCODE_COEFFICIENT);
   }
 
   decode(text) {
-    return this.applyCipher(text, 'decode');
+    return this.applyCipher(text, Cipher.DECODE_COEFFICIENT);
   }
 
-  applyCipher(text, type) {
-    const mod = (n, m) => ((n % m) + m) % m;
+  static calculateCharCode(char, shiftAmount) {
+    return (
+      mod(
+        char.charCodeAt(0) - Cipher.FIRST_CHAR_CODE + shiftAmount,
+        Cipher.POSSIBLE_KEY_CHARS.length
+      ) + Cipher.FIRST_CHAR_CODE
+    );
+  }
 
-    const calculateShiftAmount = stringIndex =>
-      this.key.charCodeAt(stringIndex % this.key.length) - Cipher.firstCharCode;
+  calculateShiftAmount(stringIndex) {
+    return (
+      this.key.charCodeAt(stringIndex % this.key.length) -
+      Cipher.FIRST_CHAR_CODE
+    );
+  }
 
-    const calculateCharCode = (char, shiftAmount) => mod(
-      char.charCodeAt(0) - Cipher.firstCharCode + shiftAmount,
-      Cipher.possible.length,
-    ) + Cipher.firstCharCode;
-
-    const shiftDirection = type === 'decode' ? -1 : 1;
-
-    return Array.from(text)
-      .map((char, index) => {
-        const shiftAmount = calculateShiftAmount(index) * shiftDirection;
-        return String.fromCharCode(calculateCharCode(char, shiftAmount));
-      })
-      .join('');
+  applyCipher(text, shiftDirection) {
+    return Array.from(text, (char, index) => {
+      const shiftAmount = this.calculateShiftAmount(index) * shiftDirection;
+      return String.fromCharCode(Cipher.calculateCharCode(char, shiftAmount));
+    }).join('');
   }
 };
 
-Cipher.possible = 'abcdefghijklmnopqrstuvwxyz';
-Cipher.randomKeyLength = 100;
-Cipher.firstCharCode = Cipher.possible.charCodeAt(0);
+Cipher.POSSIBLE_KEY_CHARS = 'abcdefghijklmnopqrstuvwxyz';
+Cipher.RANDOM_KEY_LENGTH = 100;
+Cipher.FIRST_CHAR_CODE = Cipher.POSSIBLE_KEY_CHARS.charCodeAt(0);
+Cipher.ENCODE_COEFFICIENT = 1;
+Cipher.DECODE_COEFFICIENT = -1;
 
 export default Cipher;
