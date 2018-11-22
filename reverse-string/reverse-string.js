7e9284ea7b37cf9407103c4dbcff4ffeae82231b
DaMoo <git@damoo.zone>
2018-11-21 19:43:44 -0500

    Port reverse-string exercise from TypeScript

---
+++ reverse-string/reverse-string.js
@@ -0,0 +1,9 @@
+export const reverseString = text => {
+  let returnString = "";
+  // Use an array from a string to handle Unicode properly
+  const textArray = Array.from(text);
+  for (let index = textArray.length - 1; index >= 0; index--) {
+    returnString += textArray[index];
+  }
+  return returnString;
+};
